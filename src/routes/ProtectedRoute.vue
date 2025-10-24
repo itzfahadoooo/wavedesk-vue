3041369482740ac2e8834700f646b413edafe0ef
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-10-24 17:03:46 +0100

    feat: protected route implementation

---
+++ src/routes/ProtectedRoute.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAuth } from "../stores/useAuth"
+import { useRouter } from "vue-router"
+import { watchEffect } from "vue"
+
+const router = useRouter()
+const auth = useAuth()
+
+// Watch for auth changes and redirect if not authenticated
+watchEffect(() => {
+  if (!auth.isAuthenticated) {
+    router.replace("/auth/login")
+  }
+})
+</script>
+
+<template>
+  <slot v-if="auth.isAuthenticated" />
+</template>
